c32489844899b6c09154ced3d4d35be080472a1e
Dmitry Burdakov <akaunik@gmail.com>
2014-01-29 00:21:05 +1100

    del sql

--- update.mysql.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `credentials` (`id`, `tracker`, `log`, `pass`, `cookie`)
-VALUES
-<<<<<<< HEAD
-	(9,'baibako.tv', '', '','');
-=======
-	(10,'casstudio.tv', '', '',''),
-	(11,'newstudio.tv', '', '','');
->>>>>>> upstream/master
--- update.postgresql.sql
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-INSERT INTO credentials VALUES (9, 'baibako.tv', '', '', '');
-=======
-INSERT INTO credentials VALUES (10,'casstudio.tv', '', '','');
-INSERT INTO credentials VALUES (11,'newstudio.tv', '', '','');
->>>>>>> upstream/master
